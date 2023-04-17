53423aacbd52d56057e70d301337ce31bdb43372
luangrabher <luanggcontato@gmail.com>
2023-04-17 10:40:17 -0300

    criado testes para o arquivo dashboardHelpers

---
+++ src/__tests__/dashboardHelpers.test.ts
@@ -0,0 +1,24 @@
+import { describe, test, expect, it } from 'vitest';
+import { getBackgroundColorsRandomly, getRandomColor } from '../helpers/dashboardHelpers';
+
+const colorRegex = /^#[0-9a-f]{6}$/i;
+
+describe('getBackgroundColorsRandomly', () => {
+  it('retorna um array de cores aleatórias com o tamanho passado como parâmetro', () => {
+    const colors = getBackgroundColorsRandomly(3);
+    expect(colors).toHaveLength(3);
+    expect(colors[0]).toMatch(colorRegex);
+    expect(colors[1]).toMatch(colorRegex);
+    expect(colors[2]).toMatch(colorRegex);
+    expect(colors[0]).not.toEqual(colors[1]);
+    expect(colors[0]).not.toEqual(colors[2]);
+    expect(colors[1]).not.toEqual(colors[2]);
+  });
+});
+
+describe('getRandomColor', () => {
+  it('retorna uma cor aleatória no formato hex (#FFFFFF)', () => {
+    const color = getRandomColor();
+    expect(color).toMatch(colorRegex);
+  });
+});
